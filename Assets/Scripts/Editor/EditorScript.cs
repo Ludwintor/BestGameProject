c22148443dd3292644a0d1d16513c5edc831c52f
Ludwintor <izumrud233@gmail.com>
2022-02-03 12:44:36 +0700

    Add empty project and plugins
    Added plugins: DOTween, TextMeshPro and Newtonsoft.Json
    
    Added .asmdef for runtime and editor

---
+++ Assets/Scripts/Editor/EditorScript.cs
@@ -0,0 +1,10 @@
+
+// Скрипт-пустышка, чтобы .asmdef не ругался в консоль, что нечего компилировать
+
+namespace ProjectGame.Editor
+{
+    public class EditorScript
+    {
+
+    }
+}
---
+++ Assets/Scripts/Runtime/EmptyScript.cs
@@ -0,0 +1,9 @@
+// Скрипт-пустышка, чтобы .asmdef не ругался в юнити, что нечего компилить
+
+namespace ProjectGame
+{
+    public class EmptyScript
+    {
+
+    }
+}
