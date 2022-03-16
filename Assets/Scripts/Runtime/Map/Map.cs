3945f3bc7853aad5dd33e1cca73edc99d26f546f
Ludwintor <izumrud233@gmail.com>
2022-03-16 23:34:40 +0700

    Dungeon map generation

---
+++ Assets/Scripts/Runtime/Map/Map.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ProjectGame.DungeonMap
+{
+    public class Map : IEnumerable<RoomNode>
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+        private readonly RoomNode[][] _map;
+
+        public Map(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            _map = new RoomNode[rows][];
+            for (int y = 0; y < rows; y++)
+            {
+                _map[y] = new RoomNode[columns];
+                for (int x = 0; x < columns; x++)
+                    _map[y][x] = new RoomNode(x, y);
+            }
+        }
+
+        public RoomNode GetNode(int x, int y) => _map[y][x];
+
+        public RoomNode[] GetRow(int row) => _map[row];
+
+        public IEnumerator<RoomNode> GetEnumerator()
+        {
+            for (int y = 0; y < Rows; y++)
+                for (int x = 0; x < Columns; x++)
+                    yield return GetNode(x, y);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Map/RoomNode.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectGame.DungeonMap
+{
+    public class RoomNode
+    {
+        public Vector2Int Position { get; }
+        public List<RoomNode> ChildrenNodes { get; }
+        public List<RoomNode> ParentNodes { get; }
+
+        public bool HasConnection => ChildrenNodes.Count > 0 || ParentNodes.Count > 0;
+        
+        public RoomNode(int x, int y)
+        {
+            Position = new Vector2Int(x, y);
+            ChildrenNodes = new List<RoomNode>();
+            ParentNodes = new List<RoomNode>();
+        }
+
+        public RoomNode GetMaxChild() => GetMax(ChildrenNodes);
+
+        public RoomNode GetMinChild() => GetMin(ChildrenNodes);
+
+        public RoomNode GetMaxParent() => GetMax(ParentNodes);
+        public RoomNode GetMinParent() => GetMin(ParentNodes);
+
+        private RoomNode GetMax(List<RoomNode> nodes)
+        {
+            if (nodes.Count == 0)
+                return null;
+            RoomNode max = nodes[0];
+            for (int i = 1; i < nodes.Count; i++)
+            {
+                RoomNode current = nodes[i];
+                if (current.Position.x > max.Position.x)
+                    max = current;
+            }
+            return max;
+        }
+
+        private RoomNode GetMin(List<RoomNode> nodes)
+        {
+            if (nodes.Count == 0)
+                return null;
+            RoomNode min = nodes[0];
+            for (int i = 1; i < nodes.Count; i++)
+            {
+                RoomNode current = nodes[i];
+                if (current.Position.x < min.Position.x)
+                    min = current;
+            }
+            return min;
+        }
+
+        public RoomNode GetCommonAncestor(RoomNode other, int maxDepth)
+        {
+            Debug.Assert(Position.y == other.Position.y, "To find an ancestor, nodes must be on same y");
+            RoomNode leftNode = Position.x < other.Position.x ? this : other;
+            RoomNode rightNode = Position.x < other.Position.x ? other : this;
+            int lastY = Position.y - maxDepth;
+            for (int currentY = Position.y; currentY >= lastY; currentY--)
+            {
+                if (leftNode.ParentNodes.Count == 0 || rightNode.ParentNodes.Count == 0)
+                    return null;
+                leftNode = leftNode.GetMaxParent();
+                rightNode = rightNode.GetMinParent();
+                if (leftNode == rightNode)
+                    return leftNode;
+            }
+            return null;
+        }
+    }
+}
